456455290b7b54edcee5100f8521b20cb4246ba8
Nikita Hryhorovych <71128359+HryhorovychN@users.noreply.github.com>
2022-10-17 12:00:17 +0300

    change 1/17.10.2022

--- src/main/java/commons/config/ChromeProvider.java
+++ src/main/java/commons/config/ChromeProvider.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.LocalFileDetector;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
@@ -18,9 +19,10 @@ public class ChromeProvider implements WebDriverProvider {
         chromeOptions.setAcceptInsecureCerts(true);
         chromeOptions.merge(capabilities);
         chromeOptions.setPageLoadStrategy(PageLoadStrategy.NONE);
-
         try {
-            return new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), chromeOptions);
+            RemoteWebDriver webDriver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), chromeOptions);
+            webDriver.setFileDetector(new LocalFileDetector());
+            return webDriver;
         } catch (final MalformedURLException e) {
             throw new RuntimeException("Unable to create driver", e);
         }
